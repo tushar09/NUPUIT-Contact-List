74035143e3c63cf8b9c7a546f23260df3392e470
Tushar <wtushar09@gmail.com>
2017-03-30 23:34:46 +0600

    MainActivity databinding added and toobar/actionbar removed

--- app/src/main/java/com/nupuit/nupuitcontactlist/Activity/MainActivity.java
+++ app/src/main/java/com/nupuit/nupuitcontactlist/Activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.nupuit.nupuitcontactlist.Activity;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.tool.DataBinder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,15 +12,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.nupuit.nupuitcontactlist.R;
+import com.nupuit.nupuitcontactlist.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity{
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
 
     }
 
