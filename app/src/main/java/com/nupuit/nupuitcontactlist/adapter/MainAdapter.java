530f89c0b196b18d4326f7c365ef82039344bb8a
Tushar <wtushar09@gmail.com>
2017-03-31 02:53:56 +0600

    MainAdapter completed

--- app/src/main/java/com/nupuit/nupuitcontactlist/adapter/MainAdapter.java
+++ app/src/main/java/com/nupuit/nupuitcontactlist/adapter/MainAdapter.java
@@ -1,10 +1,14 @@
 package com.nupuit.nupuitcontactlist.adapter;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.nupuit.nupuitcontactlist.R;
+import com.nupuit.nupuitcontactlist.databinding.RowContactListBinding;
 import com.nupuit.nupuitcontactlist.db.Contacts;
 
 import java.util.ArrayList;
@@ -40,10 +44,29 @@ public class MainAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
-        return null;
+
+        Holder holder;
+
+        if(convertView == null){
+            holder = new Holder();
+            convertView = holder.binding.getRoot();
+            convertView.setTag(holder);
+        }else {
+            holder = (Holder) convertView.getTag();
+        }
+
+        holder.binding.tvName.setText(contacts.get(position).getName());
+        holder.binding.tvNumber.setText(contacts.get(position).getNumber());
+
+        return convertView;
     }
 
     private class Holder{
 
+        RowContactListBinding binding;
+
+        public Holder(){
+            binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.row_contact_list, null, true);
+        }
     }
 }
